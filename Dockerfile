27902eb28f24eed0b3389831db3b3a5845b5d9f7
Miqueias <miqueias.rihs@gmail.com>
2023-08-27 17:07:27 -0300

    setup script was created

--- Dockerfile
+++ Dockerfile
@@ -2,5 +2,11 @@ FROM python:3
 
 WORKDIR /home/app/
 
+COPY .env .
+COPY entrypoint.sh .
 COPY requirements.txt .
-RUN pip install --no-cache-dir -r requirements.txt
+
+RUN pip install --no-cache-dir -r requirements.txt \
+    && chmod +x entrypoint.sh
+
+CMD ["chmod +x entrypoint.sh"]
---
+++ setup_dev.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+python manage.py makemigrations
+python manage.py migrate
+python manage.py createcachetable
+
+python manage.py createsuperuser \
+    --noinput \
+    --username $DJANGO_SUPERUSER_USERNAME \
+    --email $DJANGO_SUPERUSER_EMAIL
+
+python manage.py runserver 0.0.0.0:8000
